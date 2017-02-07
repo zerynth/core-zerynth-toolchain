3f34a734d2b37f6faf9424c7531b139b802bf8b9
Matteo <m.cipriani@zerynth.com>
2017-02-07 17:41:07 +0100

    bug fixed for command and args with spaces

--- base/proc.py
+++ base/proc.py
@@ -25,14 +25,18 @@ class prc():
             return 1,"","" #TODO: raise proper exception
         return self.run(cmdval,*args,outfn=outfn)
 
+    def escape(self,args):
+        return ["\""+x+"\"" for x in args]
+
     def run(self,cmd,*args,outfn=None):
         try:
             if isinstance(cmd,str):
-                cmd = cmd+" "+(" ".join(args))
+                cmd = "\""+cmd+"\""
+                cmd = cmd+" "+(" ".join(self.escape(args)))
             else:
                 cmd.extend(args)
-                cmd = " ".join(cmd)
-            torun = shlex.split(cmd,posix=not env.is_windows())
+                cmd = " ".join(self.escape(cmd))
+            torun = shlex.split(cmd) #posix=not env.is_windows()
             #TODO: consider swicth to Python 3.5 for subprocess.run
             try:
                 p=subprocess.Popen(torun,universal_newlines=True,stderr=subprocess.STDOUT,startupinfo=self.startupnfo,stdout=subprocess.PIPE)
@@ -50,13 +54,10 @@ class prc():
         except subprocess.CalledProcessError as e:
             return e.returncode,e.output,e.output
 
-
     def run_get_lines(self,cmd,*args):
         r,out,err = self.run(cmd,*args)
         for line in out.split("\n"):
             yield line
-
-
 
 proc = prc()
 
