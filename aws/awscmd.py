b130a2414997913b6c511c39027312751d9fc431
l.rizzello <l.rizzello@zerynth.com>
2017-10-12 13:29:35 +0200

    ztc aws doc
    Task name: aws_iot
    
    Reason for the commit: aws command doc + fixing compiler typo
    
    Changes:
    
    * ...

--- aws/awscmd.py
+++ aws/awscmd.py
@@ -2,25 +2,27 @@
 # @Author: Lorenzo
 # @Date:   2017-10-03 09:34:26
 # @Last Modified by:   Lorenzo
-# @Last Modified time: 2017-10-10 17:09:09
+# @Last Modified time: 2017-10-12 13:27:58
 
 """
 .. _ztc-cmd-aws:
 
-*******************
+===================
 Amazon Web Services
-*******************
+===================
 
-The integration between the Zerynth Toolchain and AWS command line tool allows to easily manage AWS resources 
+The integration between the Zerynth Toolchain and AWS command line tool allows to easily manage AWS resources while working on your embedded project.
 
-The following commands are available: 
+.. note:: The Zerynth Toolchain assumes `AWS command line tool <https://aws.amazon.com/cli>`_ to be available and `configured <http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html>`_ to handle AWS resources.
 
-    * AWS IoT Platform:
+Since AWS Platform provides several services, this page wil report the documentation of each ztc-aws-integrated command in a proper section depending on referred AWS service:
+
+    * :ref:`AWS IoT Platform <ztc-cmd-aws-iot_platform>`:
 
         * :ref:`thing-project-from-template <ztc-cmd-aws-thing_project_from_template>`
-        * :ref:`set-active-thing <ztc-cmd-aws-set_active_thing>`
         * :ref:`add-things <ztc-cmd-aws-add_things>`
-        * :ref:`__clean <ztc-cmd-aws-__clean>`
+        * :ref:`set-active-thing <ztc-cmd-aws-set_active_thing>`
+        * :ref:`iot-cleanup <ztc-cmd-aws-iot_cleanup>`
 
     """
 
@@ -29,19 +31,35 @@ import click
 from . import awscli
 from . import awsthing
 
-zawsconf_filename = '.zawsconf'
-thingsresources_dirname = '.aws_things_resources'
 
 @cli.group(help="ztc and AWS cli integration")
 def aws():
     pass
 
+def aws_iot():
+    """ 
+.. _ztc-cmd-aws-iot_platform:
 
-"""
-================
+****************
 AWS IoT Platform
-================
-"""
+****************
+
+AWS IoT is a managed cloud platform that lets connected devices easily and securely interact with cloud applications and other devices: `AWS IoT platform <https://aws.amazon.com/iot-platform/>`_.
+It is officially supported by Zerynth powered devices through a :ref:`specific module <awsiot>`.
+
+Zerynth Toolchain and AWS IoT Platform integration provides a full device project workflow: from single Thing creation and testing to :ref:`mass programming <ztc-cmd-aws-set_active_thing_note>` utilies for production environment.
+
+    * New project setup is made easy thanks to the available :ref:`aws-iot enabled device project template <ztc-cmd-aws-thing_project_from_template>`.
+    * Testing the project on a single device, authenticating as an on-the-fly generated AWS IoT Thing, is immediate with :ref:`add-things <ztc-cmd-aws-add_things>` command.
+    * :ref:`Scaling <ztc-cmd-aws-set_active_thing_note>` to multiple devices is allowed by the same :ref:`add-things <ztc-cmd-aws-add_things>` in combination with :ref:`set-active-thing <ztc-cmd-aws-set_active_thing>` command.
+
+Environment cleanup (both local and remote) is also possible through :ref:`iot-cleanup <ztc-cmd-aws-iot_cleanup>` command.
+
+    """
+    pass
+
+zawsconf_filename = '.zawsconf'
+thingsresources_dirname = '.aws_things_resources'
 
 @aws.command("thing-project-from-template",help="create a Zerynth project based on a default AWS IoT project template")
 @click.argument("project-name")
@@ -49,8 +67,7 @@ AWS IoT Platform
 @click.option("--aws-endpoint")
 @click.option("--aws-policy-name")
 def __thing_template(project_name, path, aws_endpoint, aws_policy_name):
-    """ 
-
+    """
 .. _ztc-cmd-aws-thing_project_from_template:
 
 Create a generic AWS IoT Thing project from template
@@ -58,14 +75,34 @@ Create a generic AWS IoT Thing project from template
 
 The command: ::
 
-    ztc thing-project-from-template project-name path
+    ztc aws thing-project-from-template project-name path
 
+Creates a Zerynth project called :samp:`project-name` inside a newly created :samp:`project-name` folder placed at provided :samp:`path`.
+
+The Zerynth project will be a clone of a template project capable of connecting to a wifi network and sending mqtt messages to the AWS IoT MQTT Broker, and made of the following files: ::
+
+    ├── main.py
+    ├── helpers.py
+    ├── readme.md
+    └── thing.conf.json
+
+Where :code:`main.py` will contain device task logic, made independent of AWS IoT Thing specific names and references which will be placed inside :code:`thing.conf.json` configuration file.
+
+The command provides also useful options: ::
+
+    --aws-endpoint      endpoint-name
+    --aws-policy-name   policy-name
+
+which allow to specify configuration parameters such as mqtt broker endpoint or devices policy, common to different Things, directly at project cloning time.
+These options simply set values inside :code:`thing.conf.json` which can also be easily edited manually.
+
+After customizing the code it will be necessary to generate an AWS IoT Thing to allow device authentication to AWS IoT Platform.
 
     """
     thing_template_path = fs.path(fs.dirname(__file__), 'templates', 'thing_template')
     dst_path = fs.path(path, project_name)
     info("Cloning template")
-    fs.copytree(thing_template_path, dst_path) # maybe softer, copytree removes tree!!!
+    fs.copytree(thing_template_path, dst_path)
     pinfo = {
         "title": project_name,
         "created_at":str(datetime.datetime.utcnow()),
@@ -84,33 +121,41 @@ The command: ::
     thing_conf['policy_name'] = aws_policy_name
     fs.set_json(thing_conf, conf_file)
 
-@aws.command("set-active-thing",help="set active thing for Zerynth project compilation")
-@click.argument("project-path", type=click.Path())
-@click.option("--thing-id", default=0, type=int)
-def __set_active_thing(project_path, thing_id):
-    zawsconf_file = fs.path(project_path, zawsconf_filename)
-    if not fs.isfile(zawsconf_file):
-        fatal('cannot set active Thing for a Zerynth project with no things bound to it')
-
-    thingsresources_dir = fs.path(project_path, thingsresources_dirname)
-    if not fs.isdir(thingsresources_dir):
-        fatal(thingsresources_dir, ' should be a folder!')
-
-    zawsconf = fs.get_json(zawsconf_file)
-    thingname = zawsconf['thing_base_name'] + '_' + str(thing_id)
-    thing_resources = fs.path(thingsresources_dir, thingname)
-    if not fs.isdir(thing_resources):
-        fatal(thing_resources, 'should be a folder!')
-
-    awsthing.Thing.set_active(project_path, zawsconf['thing_base_name'], thing_id)
-    info(thingname, 'set as active thing')
-
 
 @aws.command("add-things",help="create n new things bounded to chosen Zerynth Project")
 @click.argument("project-path", type=click.Path())
 @click.option("--thing-base-name", default='', type=str)
 @click.option("--things-number", default=1, type=int)
 def __add_things(project_path, thing_base_name, things_number):
+    """
+.. _ztc-cmd-aws-add_things:
+
+Bind AWS IoT Things to a Zerynth Project
+----------------------------------------
+
+The command: ::
+
+    ztc aws add-things project_path --things-base-name first_project_thing
+
+will generate an AWS IoT Thing called :code:`first_project_thing_0` and download Thing private key and certificate inside Zerynth project folder placed at :code:`project_path`.
+Private key and certificate among with a Thing specific :code:`thing.conf.json` will be stored inside :code:`first_project_thing_0` folder under :code:`.aws_things_resources` one placed at project top-level folder.
+
+The newly created Thing will be set as the :ref:`active <ztc-cmd-aws-set_active_thing>` one and its specific configuration will be placed inside top-level :code:`thing.conf.json` and its key and certificate copied into :code:`private.pem.key` and :code:`certificate.pem.crt` files respectively.
+
+The Project is now ready for :ref:`compilation <ztc-cmd-compile>` and :ref:`uplink <ztc-cmd-uplink>` processes.
+
+After testing the project on a single Thing it will be possible to add multiple things to the same project through: ::
+
+    ztc aws add-things project_path --things-number 10
+
+creating 10 new Things with private keys and certificates with names from :code:`first_project_thing_1` to :code:`first_project_thing_10`. 
+Otherwise it will be possible to specify a new base name calling again the proper option: ::
+
+    ztc aws add-things project_path --things-number 10 --things-base-name production_thing
+
+After creation it is needed to choose each time a single Thing for :ref:`compilation <ztc-cmd-compile>` and :ref:`uplink <ztc-cmd-uplink>`.
+
+    """
     _awscli = awscli.AWSCli()
     zawsconf_file = fs.path(project_path, zawsconf_filename)
     zawsconf = {}
@@ -150,9 +195,89 @@ def __add_things(project_path, thing_base_name, things_number):
         awsthing.Thing.set_active(project_path, thing_base_name, 0)
 
 
-@aws.command("__clean",help="destroy things and certificates (both local and remote)")
+@aws.command("set-active-thing",help="set active thing for Zerynth project compilation")
 @click.argument("project-path", type=click.Path())
-def __clean(project_path):
+@click.option("--thing-id", default=0, type=int)
+def __set_active_thing(project_path, thing_id):
+    """
+.. _ztc-cmd-aws-set_active_thing:
+
+Set a Thing as active for compilation and uplink
+------------------------------------------------
+
+The command: ::
+
+    ztc aws set-active-thing project_path --thing-id 0
+
+will set :code:`thing_base_name_0` as active Thing (:code:`first_project_thing_0` following the example above) for the Zerynth project placed at :code:`project_path`.
+The process consists in:
+
+    * copying Thing private key and certificate to top-level project folder;
+    * filling Thing specific fields inside :code:`thing.conf.json` configuration file:
+
+        * certificate `Amazon Resource Name (ARN) <http://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html>`_, 
+        * Thing name,
+        * MQTT ID to log to AWS IoT MQTT Broker (same as Thing name).
+
+.. _ztc-cmd-aws-set_active_thing_note:
+
+A note on mass programming
+--------------------------
+
+The simple script ::
+
+    #!/usr/bin/sh
+
+    things_number=1000
+    ztc=/ztc_path/ztc
+
+    $ztc aws add-things project_path --things-number $things_number --thing-base-name production_thing
+    for thing_id in $(seq 0 $(expr $things_number - 1)); do
+        $ztc aws set-active-thing project_path --thing-id $thing_id 
+        $ztc compile project_path target_device
+        $ztc uplink target_device project_path/main.vbo
+    done
+
+
+allows to scale a single Thing project to an arbitrary number of Things.
+
+    """
+    zawsconf_file = fs.path(project_path, zawsconf_filename)
+    if not fs.isfile(zawsconf_file):
+        fatal('cannot set active Thing for a Zerynth project with no things bound to it')
+
+    thingsresources_dir = fs.path(project_path, thingsresources_dirname)
+    if not fs.isdir(thingsresources_dir):
+        fatal(thingsresources_dir, ' should be a folder!')
+
+    zawsconf = fs.get_json(zawsconf_file)
+    thingname = zawsconf['thing_base_name'] + '_' + str(thing_id)
+    thing_resources = fs.path(thingsresources_dir, thingname)
+    if not fs.isdir(thing_resources):
+        fatal(thing_resources, 'should be a folder!')
+
+    awsthing.Thing.set_active(project_path, zawsconf['thing_base_name'], thing_id)
+    info(thingname, 'set as active thing')
+
+
+@aws.command("iot-cleanup",help="destroy things and certificates (both local and remote)")
+@click.argument("project-path", type=click.Path())
+def __iot_cleanup(project_path):
+    """
+.. _ztc-cmd-aws-iot_cleanup:
+
+Cleanup project deleting bound AWS IoT Things
+---------------------------------------------
+
+The command: ::
+
+    ztc aws iot-cleanup project_path
+
+Deletes AWS IoT Things bound to Zerynth project placed at :code:`project_path` and attached keys and certificates.
+
+.. warning:: clean up process acts both **locally** and **remotely**
+
+    """
     _awscli = awscli.AWSCli()
 
     thingsresources_dir = fs.path(project_path, thingsresources_dirname)
